fbcc33b22ee0d10badb55e8673c5b5955a199e0e
Zuyuan Zhu <zuyuanzhu@gmail.com>
2021-03-02 21:24:33 +0000

    test

--- src/tutorial_pkg_node.cpp
+++ src/tutorial_pkg_node.cpp
@@ -9,6 +9,10 @@
 
 #include "../include/Stopper.h"
 
+double trs = -2.8;
+double fs = 0.27;
+double mfs = 0.28;
+
 
 Stopper::Stopper(){
 	keepMoving = true;
@@ -32,6 +36,7 @@ void Stopper::moveForward(double forwardSpeed){
 void Stopper::moveStop(){
 	geometry_msgs::Twist msg; 
 	msg.linear.x = FORWARD_SPEED_STOP;
+	msg.angular.z = FORWARD_SPEED_STOP;
 	commandPub.publish(msg);
 }
 
@@ -44,47 +49,13 @@ void Stopper::moveLeft(double turn_left_speed){
 void Stopper::moveRight(double turn_right_speed){
 	geometry_msgs::Twist msg; 
 	msg.angular.z = turn_right_speed;
+	msg.linear.x = fs;
 	commandPub.publish(msg);
 }
 
-// test
-void Stopper::keepMiddleTorward2ndGap(unsigned int rightClosestRangeIndex, unsigned int leftClosestRangeIndex){
-	int middleIndex;
-	middleIndex = ceil((720-rightClosestRangeIndex - leftClosestRangeIndex)/2.0);
-	ROS_INFO_STREAM("middleIndex " << middleIndex);
-	if(middleIndex > 20){
-		moveRight(TURN_RIGHT_SPEED_LOW);
-		ROS_INFO_STREAM("Move right a little ");
-	}
-	else if((middleIndex < -10)){
-		//moveLeft(TURN_LEFT_SPEED_LOW);
-		ROS_INFO_STREAM("Move left a little ");
-	}
-	else{
-		moveForward(FORWARD_SPEED_LOW/2.0);
-		ROS_INFO_STREAM("Move forward a little ");
-	}
-		
-}
 
-void Stopper::keepMoveAlongWall(double range2Wall){
-	ROS_INFO_STREAM("leftRange to the Wall: " << leftRange);
-	if(leftRange > (range2Wall+0.2)){
-		moveLeft(TURN_LEFT_SPEED_HIGH);
-		ROS_INFO_STREAM("Turn left a little ");
-		displayMessages();			
-	} 
-	if(leftRange < (range2Wall-0.2)){
-		moveRight(TURN_RIGHT_SPEED_MIDDLE);
-		ROS_INFO_STREAM("Turn right a little ");
-		displayMessages();
-	}
-	else{
-		moveForward(FORWARD_SPEED_LOW);	
-		ROS_INFO_STREAM("Move forward a little ");	
-		displayMessages();
-	}
-}
+
+
 
 /*   not working
 int Stopper::findClosestRangeIndex(int indexRangeMin, int indexRangeMax, const sensor_msgs::LaserScan::ConstPtr& laserScan){
@@ -109,20 +80,12 @@ void Stopper::displayMessages(){
 		ROS_INFO_STREAM("frontRange: " << frontRange); 
 		ROS_INFO_STREAM("rearRange: " << rearRange);
 		ROS_INFO_STREAM("robotHeadAngle: " << robotHeadAngle); 
+		ROS_INFO_STREAM("robotPoseX: " << robotPoseX); 
+		ROS_INFO_STREAM("robotPoseY: " << robotPoseY); 
 }
 
 
-// transform the points to golable coordination, create the map of the environment, write in the txt file
-void Stopper::transformMapPoint(ofstream& fp, double _obstacleRange, double _sonarX, double _sonarY,
-	double _sonarTh, double _robotTh, double _globalX, double _globalY)
-{
-	double transX, transY;
-	transX = (_obstacleRange * cos(_sonarTh) + _sonarX) * cos(_robotTh)- (_obstacleRange * sin(_sonarTh) + _sonarY) * sin(_robotTh) + _globalX;
-		    
-	transY = (_obstacleRange * cos(_sonarTh) + _sonarX) * sin(_robotTh) + (_obstacleRange * sin(_sonarTh) + _sonarY) * cos(_robotTh) + _globalY;	
-		
-	fp << setprecision(3)<< transX <<"  "<< setprecision(3) << 	transY << endl;
-}
+
 
 
 
@@ -149,9 +112,9 @@ void Stopper::odomCallback(const nav_msgs::Odometry::ConstPtr& odomMsg){
   	
   	robotPoseX = odomMsg->pose.pose.position.x;
   	robotPoseY = odomMsg->pose.pose.position.y;
-  	transformMapPoint(odomMapFile, frontRange, 0, 0, 0, robotAngles.yaw, robotPoseX, robotPoseY);
-  	transformMapPoint(odomMapFile, rightRange, 0, 0, -1.57, robotAngles.yaw, robotPoseX, robotPoseY);
-  	transformMapPoint(odomMapFile, leftRange, 0, 0, 1.57, robotAngles.yaw, robotPoseX, robotPoseY);
+  	//transformMapPoint(odomMapFile, frontRange, 0, 0, 0, robotAngles.yaw, robotPoseX, robotPoseY);
+  	//transformMapPoint(odomMapFile, rightRange, 0, 0, -1.57, robotAngles.yaw, robotPoseX, robotPoseY);
+  	//transformMapPoint(odomMapFile, leftRange, 0, 0, 1.57, robotAngles.yaw, robotPoseX, robotPoseY);
 }
 
 
@@ -164,18 +127,6 @@ void Stopper::scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan){
 	//	Find the closest range between the defined minimum and maximum angles
 	ros::Rate rate(20);
 	
-	/*
-	int	minIndex = ceil((LEFT_SCAN_ANGLE_RAD - scan->angle_min) / scan->angle_increment);  // the transfermation is not right
-	int	maxIndex = floor((RIGHT_SCAN_ANGLE_RAD - scan->angle_min) / scan->angle_increment);
-	int frontIndex = floor((0 - scan->angle_min) / scan->angle_increment);		
-	float closestRange = scan->ranges[minIndex];
-	for (int currIndex = minIndex + 1; currIndex <= maxIndex; currIndex++){
-		if (scan->ranges[currIndex] < closestRange){
-			closestRange = scan->ranges[currIndex];
-		}
-	}
-	*/
-	
 	frontRange = scan->ranges[0];
 	rightRange = scan->ranges[539];
 	leftRange = scan->ranges[180];
@@ -185,16 +136,35 @@ void Stopper::scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan){
 	********** Put your code here **************
 	********************************************
 	*/
-	
+
 	// STAGE = 0, Initial stage, start moving towards the first gap before turing right
 	if (((rightRange < 0.38)||(frontRange < 1.0)) && STAGE==0){
 		ROS_INFO("Turn Right");
-		moveRight(TURN_RIGHT_SPEED_HIGH);
-		//displayMessages();
+		moveRight(trs);
+		displayMessages();
 	}
-	// finished first turning
 	
+	// finished first turning, if robot head angle is 0, i.e., heading towards down
+	if( (-0.09 < robotHeadAngle && robotHeadAngle < 0.09) && (frontRange < 2.5 ) &&( rearRange> 1)){
+	     STAGE = 1; 
+	    // stop turning right, move forward
+	    ROS_INFO_STREAM("==========stop turning right ");
+	    moveStop();
+	    //ros::shutdown();
+	    moveForward(FORWARD_SPEED_LOW);
+	    displayMessages();
+	    // arriving at the centre of gap 1 (0.5, 1.575), stop
+	    if ( robotPoseX > 0.45  ) {
+	        moveStop();
+	        ros::shutdown();
+	    }
+	    
+	    if(( robotPoseX > 0.45 && robotPoseX < 0.55) && (robotPoseY > 1.525 && robotPoseY < 1.625)){
+	    moveStop();
+	    }
+	}
 	//If the robot is in the middle of the first gate, keep moving forward
+	/*
 	if( (0.2 <leftRange) && (leftRange < 0.64) &&  (0.2 < rightRange) && (rightRange < 0.35) && (1.8< frontRange) && (frontRange < 1.9)){
 		STAGE = 1;
 
@@ -217,117 +187,27 @@ void Stopper::scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan){
 			STAGE = 3;				
 		}
 	}
-	if((STAGE==3)){
-		moveForward(FORWARD_SPEED_LOW);			
-		/*
-		********************************************
-		****** find the closest range index ********
-		*************** right side *****************
-		********************************************
-		*/
-		int indexRightRangeMin = 600;
-		int indexRightRangeMax = 719;
-		float closestRightRange = scan->ranges[indexRightRangeMin];
-		int closestRightRangeIndex = indexRightRangeMin;
-		for (int currRightIndex = indexRightRangeMin + 1; currRightIndex <= indexRightRangeMax; currRightIndex++){
-			if (scan->ranges[currRightIndex] < closestRightRange){
-				closestRightRange = scan->ranges[currRightIndex];
-				closestRightRangeIndex = currRightIndex;
-			}
-		}
-		ROS_INFO_STREAM("closestRightRangeIndex: " << closestRightRangeIndex <<" "<< closestRightRange);		
-		/*
-		********************************************
-		****** find the closest range index ********
-		*************** left  side *****************
-		********************************************
-		*/
-		int indexLeftRangeMin = 0;
-		int indexLeftRangeMax = 150;
-		float closestLeftRange = scan->ranges[indexLeftRangeMin];
-		int closestLeftRangeIndex = indexLeftRangeMin;
-		for (int currLeftIndex = indexLeftRangeMin + 1; currLeftIndex <= indexLeftRangeMax; currLeftIndex++){
-			if (scan->ranges[currLeftIndex] < closestLeftRange){
-				closestLeftRange = scan->ranges[currLeftIndex];
-				closestLeftRangeIndex = currLeftIndex;
-			}
-		}
-		ROS_INFO_STREAM("closestLeftRangeIndex: " << closestLeftRangeIndex <<" "<< closestLeftRange);		
-		
-		// Adjust heading angle torwards the 2nd gap
-		if ((rightRange > 0.25) && (leftRange > 0.25)){  // make sure the robot won't adjust head angle when nearby the pillars
-			keepMiddleTorward2ndGap(closestRightRangeIndex,closestLeftRangeIndex);
-		}		
-		displayMessages();
-		
-		// keep straight going through the 2nd gap
-		if((closestLeftRange < 0.22)&& (closestRightRange < 0.22)){
-			STAGE = 4;			// go through 2nd gap		
-			displayMessages();				
-		}
-	}
-	if((rightRange < 0.5 ) && (frontRange > 0.5) && (STAGE==4)){
-		moveForward(FORWARD_SPEED_LOW);			//go through 2nd gap		
-		displayMessages();			
-	}
-	else if((frontRange < 0.5)&& (STAGE==4)){ 
-		STAGE = 5;	//passed 2nd gap, prepare to turn right	 		
-	}
-	if(((rightRange > 1.2)&& (rightRange < 2.2)) && (STAGE == 5)){
-		moveRight(TURN_RIGHT_SPEED_MIDDLE); // turn right
-		displayMessages();	
-		if((frontRange > 0.8) && (leftRange < 0.335) && ((rightRange > 2.0)||(rightRange < 0.55))){
-			STAGE = 6; // stop turning right, prepare to move forward along wall bottom
-		}	
-	}			
-	if( (frontRange > 0.32) && (STAGE == 6)){
-		moveForward(FORWARD_SPEED_LOW);
-		double leftRange2Wall = 0.32;
-		keepMoveAlongWall(leftRange2Wall); // move forward, parallel to the left wall	
-		displayMessages();
-	}
-	else if( (frontRange < 0.32) && (STAGE == 6)){
-		STAGE = 7;  // turn right, parallel to the wall (0,0) - (2.5, 0)
-		displayMessages(); 				
-	}
-	if((frontRange < 2.2)&& (STAGE == 7)){  // turn right, parallel to the wall (0,0) - (2.5, 0)		
-		moveRight(TURN_RIGHT_SPEED_MIDDLE);
-		displayMessages(); 
-	    if((leftRange < 0.35) && (leftRange > 0.32)  && (frontRange > 1.7) && (frontRange < 2.2) && ((rightRange < 1.0)||(rightRange > 2.0))){
-	    	STAGE = 8;  // Stop turning right, prepare to move forward
-			displayMessages();  	
-	    } 	
-	}
-	if (STAGE == 8){
-		moveForward(FORWARD_SPEED_HIGH);
-		double leftRange2Wall2Chagrer = 0.32;
-		keepMoveAlongWall(leftRange2Wall2Chagrer); // move forward, parallel to the left wall
-		if((frontRange < 1.2) && (frontRange > 1.1)){
-			STAGE = 9;
-			moveStop();
-			displayMessages(); 
-			ROS_INFO_STREAM("ARRIVING AT CHARGER STATION");					
-		}			
-	}						
+	*/
+							
 }
 
 void Stopper::startMoving(){
 	ros::Rate rate(20);
 	ROS_INFO("Start moving");
-	odomVelFile.open("/home/zuyuan/ros_ws/src/tutorial_pkg/odomVelData.csv",ios::out | ios::app);
-	odomMapFile.open("/home/zuyuan/ros_ws/src/tutorial_pkg/odomMapData.csv",ios::out | ios::app);
+	moveForward(mfs);
+	//odomVelFile.open("/home/zuyuan/ros_ws/src/tutorial_pkg/odomVelData.csv",ios::out | ios::app);
+	//odomMapFile.open("/home/zuyuan/ros_ws/src/tutorial_pkg/odomMapData.csv",ios::out | ios::app);
 	// keep spinning loop until user presses Ctrl+C
 	while (ros::ok() && keepMoving){
-		if(STAGE ==0){  // Comment before public to student
-			moveForward(FORWARD_SPEED_LOW);
-			displayMessages();
-			
-		}
+	if (STAGE==0){
+	    moveForward(mfs);
+	   // displayMessages();
+	}
 		ros::spinOnce(); // Allow ROS to process incoming messages
 		rate.sleep();   // or loop_rate.sleep();
 	}
-	odomVelFile.close();
-	odomMapFile.close();
+	//odomVelFile.close();
+	//odomMapFile.close();
 }
 
 int main(int argc, char **argv)
@@ -338,6 +218,7 @@ int main(int argc, char **argv)
    
    Stopper stopper;
    beginTime = ros::Time::now().toSec(); // start counting time, for velocity figure
+   ros::Duration(3).sleep(); // wait for 3 seconds for Gazebo to spawn
    // Start the movement
    stopper.startMoving();
 	
